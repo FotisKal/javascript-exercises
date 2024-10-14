4977e17c4bc4a5fe2bf37be2de578d401d27f24b
F Kal <fotis_kalokiris@outlook.com>
2024-10-14 17:31:56 +0300

    Create reverse string function

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let letters = string.split('');
+    let reversedString = '';
+    let maxIndex = string.length - 1;
 
+    for (let i = 0; i < string.length; i++) {
+        reversedString += letters[maxIndex - i]
+    }
+
+    return reversedString;
 };
 
 // Do not edit below this line
